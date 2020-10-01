8641cdb5e3a5eff58ab491a0fff8f1dd42d4a93b
Pretam <pretamroy309@gmail.com>
2020-10-01 19:43:06 +0530

    Fixed bug in problem 24

--- python/problem24.py
+++ python/problem24.py
@@ -1,33 +1,11 @@
-import math
+from math import factorial
 
 
-def swap(nums, i, j):
-    temp = nums[i]
-    nums[i] = nums[j]
-    nums[j] = temp
+def perm(n, s):
+    if len(s) == 1:
+        return s
+    q, r = divmod(n, factorial(len(s)-1))
+    return s[q] + perm(r, s[:q] + s[q+1:])
 
 
-def reverse(nums, start):
-    nums[start:] = reversed(nums[start:])
-
-
-class Solution:
-    def nextPermutation(self, nums):
-        """
-        Do not return anything, modify nums in-place instead.
-        """
-        pos = len(nums)-2
-        while(pos >= 0 and nums[pos+1] <= nums[pos]):
-            pos -= 1
-        if(pos >= 0):
-            j = len(nums)-1
-            while(j >= 0 and nums[j] <= nums[pos]):
-                j -= 1
-            swap(nums, pos, j)
-        reverse(nums, pos+1)
-        print(nums)
-
-
-s = Solution()
-for i in range(1, math.factorial(10)+1):
-    s.nextPermutation([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+print(perm(int(input("Which number of permutation? "))-1, '0123456789'))
