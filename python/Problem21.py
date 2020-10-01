a75c136a2d29b15ee195f73dcbe701e289ea133d
abhirockstar <palaskar098@gmail.com>
2020-10-01 11:38:29 +0530

    Problem 21 Solution in Python

---
+++ python/Problem21.py
@@ -0,0 +1,11 @@
+from math import sqrt
+L = 11000
+ds = [1]*L
+for i in range(2, int(sqrt(L))):
+    for j in range(i+1, L//i):
+        ds[i*j]+= i+j
+an = []
+for i in range(2, L):
+    if ds[i] < i and ds[ds[i]] == i: an+= [ds[i], i]
+N = int(input("Limit? "))
+print "Amicable sum <",N,"=", sum(a for a in an if a<N)
