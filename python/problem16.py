51e5c244fa0af749bb4ad016cc99db3fd4c4d094
anay121 <anay.kulkarni10@gmail.com>
2020-10-01 11:35:08 +0530

    Solution to problem 16 in Python

---
+++ python/problem16.py
@@ -0,0 +1,5 @@
+# To find the sum of digits of the number 2^1000
+
+#splitting each digit and mapping it to an integer, and finally passing it to a sum function
+s = sum(map(int, str(2**1000)))  
+print(s)
