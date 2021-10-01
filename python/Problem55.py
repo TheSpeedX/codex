46f816e093edc5059bab658f68cbe060d65112ff
Sweta Snigdha Sahoo <46675936+swetasahoo29@users.noreply.github.com>
2021-10-02 02:04:00 +0530

    Problem55.py

---
+++ python/Problem55.py
@@ -0,0 +1,11 @@
+def is_palindromic(n): n=str(n); return n==n[::-1]
+L = 10000
+
+def is_lychrel(n, depth=49):
+    for _ in xrange(depth):
+        n+= int(str(n)[::-1])
+        if is_palindromic(n): return 0
+    return 1
+
+print " Lychrel numbers below", L, "=",
+print sum(is_lychrel(n) for n in range(10, L))
